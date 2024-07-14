ec39cd12786e48a706c7b602a0cf3b224605202d
Beaskyy <afeez.akintobi@yahoo.com>
2024-07-14 22:13:24 +0100

    products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const router = express.Router();
 const {
   getAllProducts,
   getAllProductsStatic,
 } = require("../controllers/products");
-const router = express.Router();
 
 router.route("/").get(getAllProducts);
 router.route("/static").get(getAllProductsStatic);
+
+module.exports = router;
