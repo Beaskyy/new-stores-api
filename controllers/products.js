ebb16922b6d0bf6bef6edd575b556ea795a76d87
Beaskyy <afeez.akintobi@yahoo.com>
2024-07-16 20:15:33 +0100

    query parameters

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,20 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true, price: 120 });
+  const products = await Product.find({ featured: true });
   res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json({ products });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+    console.log(queryObject);
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, length: products.length });
 };
 
 module.exports = {
